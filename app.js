ce797034fa45e6785f6e53d66c1b823aab8b1ad1
Mohammad Asif Siddique <mdasifsidd@gmail.com>
2022-10-12 10:45:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const toggleBtn = document.querySelector(".btn");
+const articlesContainer = document.querySelector(".articles");
+// console.log(articles);
+
+toggleBtn.addEventListener("click" , ()=>{
+    document.documentElement.classList.toggle("dark-theme")
+});
+articleData = articles.map((article)=>{
+ const{title,date,length,snippet}=article;
+
+ const formatDate = moment(date).format("MMMM Do,YYYY");
+ return `<article class="post">
+  <h2>${title}</h2>
+  <div class="post-info">
+    <span>${formatDate}</span>
+    <span>${length}</span>
+  </div>
+  <p>${snippet}</p>
+ </article>`;
+}).join(" ");
+
+articlesContainer.innerHTML = articleData;
